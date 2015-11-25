1060fd2e180ed11ede5c094a1951ef13ca01fe22
k-yamank <k-yamank@ist.osaka-u.ac.jp>
2015-11-25 12:24:21 +0900

    ui to playscreen

--- js/title_layout.js
+++ js/title_layout.js
@@ -1,6 +1,6 @@
 
 // initial layout
-$(function(){
+$(document).ready(function(){
 	var w = $(window).width();
 	var h = $(window).height();
 	if(w < h){
@@ -54,17 +54,19 @@ $(window).resize(function(){
 			"height": (h -(2/3 * w))/2
 		});
 		$("#main").css({
-			"width": w,
 			"height": 2/3 * w		
 		});
 		$("#main_in").css({
-			"width": w,
-			"height": 2/3 * w
+			"width": w, 
 		})
 		$("#footer-bk").css({
 			"width": w,
 			"height": (h -(10/15 * w))/2
-		});	
+		});
+
+		$("#name").css({
+			"font-size": w * 0.02
+		})	
 	}else{
 		$("#header-bk").css({
 			"width": w,
@@ -75,11 +77,15 @@ $(window).resize(function(){
 		});
 		$("#main_in").css({
 			"width": 3/2 * (h - 100 - 100)
-		})
+		});
 		$("#footer-bk").css({
 			"width": w,
 			"height": 100	
 		});
+
+		$("#name").css({
+			"font-size": 3/2 * (h - 100 - 100) * 0.02
+		})
 	}
 });
 
