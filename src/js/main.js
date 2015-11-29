f675177ce117a43ea9cf3eb7356200e13a0c9f76
nyamadori <nyamadorig@gmail.com>
2015-11-29 17:56:35 +0900

    未リファクタリングの既存のタイトル画面を移植

---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+// entry point
+
+var $ = require('jquery');
+require('jquery.cookie');
+require('jquery.easing');
+require('animsition');
+
+$('#page-container').html(require('../html/title.html'));
+
+require('./title_layout');
+require('./title_function');
+
+
+// animsition setting
+$(document).ready(function(){
+	$(".animsition").animsition({
+		inClass: "fade-in",
+		outClass: "fade-out",
+		inDuration: 1000,
+		outDuration: 1000,
+		linkElement: ".animsition-link",
+	});
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+var path = require('path');
+var webpack = require('webpack');
+
 module.exports = {
   entry: './src/js/main.js',
   output: {
@@ -9,5 +12,21 @@ module.exports = {
     loaders: [
       { test: /\.html/, loader: 'html?minimize' }
     ],
-  }
+  },
+  resolve: {
+    root: [path.join(__dirname, "bower_components")],
+    alias: {
+      'jquery.cookie': path.join(__dirname, "bower_components/jquery.cookie/jquery.cookie.js")
+    }
+  },
+  plugins: [
+    new webpack.ResolverPlugin(
+      new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["main"])
+    ),
+    new webpack.ProvidePlugin({
+      jQuery: "jquery",
+      $: "jquery",
+      jquery: "jquery"
+    })
+  ]
 }
